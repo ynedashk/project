0bd32d76f665c32fbe67d7f57d03f60d79a1eecf
Yana <nedashk@gmail.com>
2021-04-06 15:41:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам є 18?", "");
+//console.log(answer);
+const user = 'Yana'
+alert(`Привіт, ${user}`);
